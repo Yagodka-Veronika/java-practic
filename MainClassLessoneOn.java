8d2a0271404005e1c20f212e35ef1e1db99c4a76
Veronika Shvarts <yagodka-veronika@mail.ru>
2023-06-15 20:39:19 +0500

    HW2/1

--- MainClassLessoneOn.java
+++ MainClassLessoneOn.java
@@ -1,18 +1,18 @@
 package lesson1;
-
+import java.util.Arrays;
 public class MainClassLessoneOn {
     public static void main(String[] args) {
 
-        // Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
+//         Дан массив двоичных чисел, например [1,1,0,1,1,1], вывести максимальное количество подряд идущих 1.
 
-        int[] nums = new int[] {1,1,0,1,1,1,1,0,1};
+        int[] arr = new int[] {1,1,0,1,1,1,1,0,1};
 
         int count = 0;
         int max = 0;
         int temp = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 1) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
                 count++;
             }
             else {
@@ -31,3 +31,9 @@ public class MainClassLessoneOn {
 
     }
 }
+
+
+
+
+
+
--- test3.java
+++ test3.java
@@ -1,11 +1,37 @@
 package lesson1;
 
 public class test3 {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
 
-// Напишите метод, который находит самую длинную строку общего префикса среди массива строк.
+    // Напишите метод, который находит самую длинную строку общего префикса среди массива строк.
 // Если общего префикса нет, вернуть пустую строку "".
+    private static int two(String[] arr) {
+        if (arr.length == 0) {
+            return 0;
 
-
+        }
+        else if (arr.length == 1) {
+            return arr.length;
+        }
+        else {
+            String rez = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                String cur = arr[i];
+                int reader = 0;
+                int lastCommon = 0;
+                while (reader < rez.length() && reader < cur.length()) {
+                    if (rez.charAt(reader) == cur.charAt(reader)) {
+                        lastCommon++;
+                    }
+                    else {
+                        break;
+                    }
+                    reader++;
+                }
+                rez = rez.substring(0, lastCommon);
+            }
+            return rez.length();
+        }
     }
 }
+
